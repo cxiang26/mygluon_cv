1917361b6a6cb494afe91395dabee49bbde2bc20
Shaun <cq.xiang@foxmail.com>
2019-08-06 13:45:04 +0800

    fix protonet bug that lead to unconverge

--- gluoncv/nn/protomask.py
+++ gluoncv/nn/protomask.py
@@ -13,15 +13,19 @@ class Protonet(gluon.HybridBlock):
             self.net = gluon.nn.HybridSequential(prefix='protonet_')
             self.mask = gluon.nn.HybridSequential(prefix='mask_')
             for i, channel in enumerate(self.channels):
-                if i < len(self.channels):
-                    self.net.add(gluon.nn.Conv2D(channels=channel, kernel_size=(3,3), strides=(1,1), padding=(1, 1), use_bias=False))
+                if i < len(self.channels)-2:
+                    self.net.add(gluon.nn.Conv2D(channels=channel, kernel_size=(3,3), strides=(1,1), padding=(1, 1)))
                     self.net.add(gluon.nn.Activation('relu'))
-                    self.net.add(gluon.nn.BatchNorm())
+                    # self.net.add(gluon.nn.BatchNorm())
+                elif i == len(self.channels) - 2:
+                    self.mask.add(gluon.nn.Conv2DTranspose(in_channels=channel, channels=channel, kernel_size=(2,2),strides=(2,2), groups=channel))
+                    self.mask.add(gluon.nn.Conv2D(channels=channel, kernel_size=(3, 3), strides=(1, 1), padding=(1, 1)))
+                    self.mask.add(gluon.nn.Activation('relu'))
                 else:
                     self.mask.add(gluon.nn.Conv2D(channels=channel, kernel_size=(1,1), strides=(1,1), padding=(0, 0), use_bias=False))
                     self.mask.add(gluon.nn.Activation('relu'))
     def hybrid_forward(self, F, x):
         x = self.net(x)
-        x = F.UpSampling(x, scale=self.upsampling_ratio, sample_type='nearest')
+        # x = F.UpSampling(x, scale=self.upsampling_ratio, num_filter=3, sample_type='bilinear')
         x = self.mask(x)
         return x
