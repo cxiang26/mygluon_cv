ae55687faeda8966e8abb919a2b7d20c5aa7c8a9
Shaun <cq.xiang@foxmail.com>
2019-09-15 14:25:47 +0800

    transform deconv to upsample operation

--- gluoncv/nn/protomask.py
+++ gluoncv/nn/protomask.py
@@ -21,7 +21,7 @@ class Protonet(gluon.HybridBlock):
                     self.net.add(gluon.nn.Activation('relu'))
                     # self.net.add(gluon.nn.BatchNorm())
                 elif i == len(self.channels) - 2:
-                    self.mask.add(gluon.nn.Conv2DTranspose(in_channels=channel, channels=channel, kernel_size=(2,2),strides=(2,2), groups=channel))
+                    # self.mask.add(gluon.nn.Conv2DTranspose(in_channels=channel, channels=channel, kernel_size=(2,2),strides=(2,2), groups=channel))
                     self.mask.add(gluon.nn.Conv2D(channels=channel, kernel_size=(3, 3), strides=(1, 1), padding=(1, 1)))
                     self.mask.add(gluon.nn.Activation('relu'))
                 else:
@@ -29,6 +29,7 @@ class Protonet(gluon.HybridBlock):
                     self.mask.add(gluon.nn.Activation('relu'))
     def hybrid_forward(self, F, x):
         x = self.net(x)
+        x = F.contrib.BilinearResize2D(x, scale_height=2, scale_width=2)
         # x = F.UpSampling(x, scale=self.upsampling_ratio, num_filter=3, sample_type='bilinear')
         x = self.mask(x)
         return x
